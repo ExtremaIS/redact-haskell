b954eaeaf731463f929f2a032925b28a4b7019c3
Travis Cardwell <travis.cardwell@extrema.is>
2022-02-01 06:31:59 +0900

    Use onException

--- app/Main.hs
+++ app/Main.hs
@@ -22,7 +22,7 @@ import Text.PrettyPrint.ANSI.Leijen (Doc)
 
 -- https://hackage.haskell.org/package/base
 import Control.Applicative ((<|>), optional)
-import Control.Exception (SomeException, handle)
+import Control.Exception (onException)
 import Control.Monad (foldM, forM_, when)
 import Data.Bifunctor (Bifunctor(bimap))
 import Data.Char (isSpace, toLower)
@@ -397,7 +397,7 @@ errorExit message = do
     exitWith $ ExitFailure 1
 
 main :: IO ()
-main = handle onAnyException $ do
+main = flip onException RedactTerm.reset $ do
     Options{..} <- getOptions
     let color     = runIdentity optColor
         intensity = runIdentity optIntensity
@@ -412,6 +412,3 @@ main = handle onAnyException $ do
       Nothing
         | lenient   -> Right <$> Redact.handleToTerminal' sgrs stdin
         | otherwise ->           Redact.handleToTerminal  sgrs stdin
-  where
-    onAnyException :: SomeException -> IO ()
-    onAnyException _e = RedactTerm.reset
